1f02cc8039db063a9b256a305e2e7c1123108a36
Hernane Tavares <hernane_d@hotmail.com>
2024-08-16 09:41:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-var teste = 1;
-console.log(teste);
+function armazenarValor() {
+
+    // Armazena o valor que o usuário digitou
+    var userInput = document.getElementById("userInput").value;
+
+
+    // Exibe o valor armazenado
+    console.log("O valor da variável é: " + userInput)
+
+
+    // Atualiza o valor
+    document.getElementById("valorInserido").innerText = "O Valor Inserido é = " + userInput;
+}
